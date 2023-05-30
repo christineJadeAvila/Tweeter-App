8bd91fad368b39553309dbb20b8bcc4b4ab480b7
christineJadeAvila <christinejadeavila05@gmail.com>
2023-05-30 21:05:24 +0800

    added auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import './scss/index.scss'
 import { Home } from './pages/Home'
 import { Profile } from './pages/Profile'
 import { Messages } from './pages/Messages'
+import { Auth } from './pages/Auth'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Home/>
-      <Profile/> */}
-      <Messages/>
+      <Auth />
+      {/* <Profile/>
+      <Messages/> */}
     </>
   )
 }
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import '../scss/auth.scss'
 
 export const Auth = () => {
   return (
-    <div>Auth</div>
+    <div className='flex--container'> 
+
+      <h1>Welcome to tweeter</h1>
+
+      <nav className="navigation--bar">
+        <h2>Sign In</h2>
+        <h2>| Sign Up</h2>
+        <img src="" alt="" />
+      </nav>
+
+      <form action="" className="inputs">
+          <input type="email" name="email" id="" placeholder="email"/>
+          <input type="text" name="username" id="" placeholder="username"/>
+          <input type="password" name="password" id="" placeholder="password"/>
+      </form>
+
+      <footer className='footer'>
+          <a href="">already have an account?</a>
+          <a href=""><button>sign up</button></a>
+      </footer>
+
+    </div>
   )
 }
